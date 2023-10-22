0aa086dd3a83d8cb63380be2e1b6dd38be32cfdb
rostikcheremys <rostikcheremys@gmail.com>
2023-10-22 15:22:26 +0300

    Initial commit

---
+++ Program/MyFrac.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Program
+{
+    public class MyFrac
+    {
+        private readonly long _numerator;
+        private readonly long _denominator;
+        private readonly int _pointer;
+
+        public MyFrac(long n, long d)
+        {
+            if ((n < 0 && d > 0) || (n > 0 && d < 0))
+            {
+                _pointer = 1;
+            }
+          
+            if (n != 0)
+            {
+                long divider = BiggestDivider(Math.Abs(n), Math.Abs(d));
+                
+                _numerator = n / divider;
+                _denominator = d / divider;
+            }
+            else
+            {
+                _numerator = n;
+                _denominator = d;
+            }
+        }
+
+        private long BiggestDivider(long numerator, long denominator)
+        {
+            while (denominator != numerator)
+            {
+                if (denominator > numerator)
+                {
+                    denominator -= numerator;
+                }
+                else
+                {
+                    numerator -= denominator;
+                }
+            }
+
+            return numerator;
+        }
+
+        public string ToStringWithIntegerPart()
+        {
+            if (Math.Abs(_numerator) <= Math.Abs(_denominator)) return ToString();
+            
+            if (_numerator % _denominator == 0)
+            {
+                return ToString();
+            }
+
+            long integerPart = _numerator / _denominator;
+                
+            if (_pointer == 1)
+            {
+                return $"-({Math.Abs(integerPart)} + {Math.Abs(_numerator) % Math.Abs(_denominator)}/{Math.Abs(_denominator)})";
+            }
+                    
+            return $"({integerPart} + {_numerator % _denominator}/{_denominator})";
+
+        }
+
+        public double DoubleValue()
+        {
+            if (_pointer == 1)
+            {
+                return -(Convert.ToDouble(_numerator) / Convert.ToDouble(_denominator));
+            }
+            
+            return Convert.ToDouble(_numerator) / Convert.ToDouble(_denominator);
+        }
+
+        public MyFrac Plus(MyFrac frac)
+        {
+            return new MyFrac(_numerator * frac._denominator + _denominator * frac._numerator, _denominator * frac._denominator);
+        }
+
+        public MyFrac Minus(MyFrac frac)
+        {
+            return new MyFrac(_numerator * frac._denominator - _denominator * frac._numerator, _denominator * frac._denominator);
+        }
+
+        public MyFrac Multiply(MyFrac frac)
+        {
+            return new MyFrac(_numerator * frac._numerator, _denominator * frac._denominator);
+        }
+
+        public MyFrac Divide(MyFrac frac)
+        {
+            return new MyFrac(_numerator * frac._denominator, _denominator * frac._numerator);
+        }
+
+        public MyFrac CalcSumFirst(int number)
+        {
+            MyFrac result = new MyFrac(0, 1);
+
+            for (int i = 1; i <= number; i++)
+            {
+                MyFrac adding = new MyFrac(1, i * (i + 1));
+
+                result = result.Plus(adding);
+            }
+
+            return result;
+        }
+
+        public MyFrac CalcSumSecond(int number)
+        {
+            MyFrac result = new MyFrac(1, 1).Minus(new MyFrac(1, 4));
+
+            for (int i = 3; i <= number; i++)
+            {
+                MyFrac adding = new MyFrac(1, 1).Minus(new MyFrac(1, i * i));
+
+                result = result.Multiply(adding);
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            if (_numerator == 0) return "0";
+               
+            if (_numerator == _denominator && _pointer == 0) return "1";
+                
+            if (Math.Abs(_numerator) == Math.Abs(_denominator) && _pointer == 1) return "-1";
+                
+            return $"{_numerator}/{_denominator}";
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        private static void CheckLength(string[] number)
+        {
+            if (number.Length != 2)
+            {
+                throw new FormatException("Введiть два числа через пробiл!");
+            }
+        }
+        
+        public static void Main()
+        {
+            Console.Write("Перший дрiб:\nВведiть чисельник та знаменник другого дробу через пробiл: ");
+            string[] firstFraction = Console.ReadLine().Trim().Split();
+
+            CheckLength(firstFraction);
+            
+            int firstNumerator = int.Parse(firstFraction[0]);
+            int firstDenominator = int.Parse(firstFraction[1]);
+            
+            MyFrac fracOfFirst = new MyFrac(firstNumerator, firstDenominator);
+            
+            Console.Write("\nДругий дрiб:\nВведiть чисельник та знаменник другого дробу через пробiл: ");
+            string[] secondFraction = Console.ReadLine().Trim().Split();
+            
+            CheckLength(secondFraction);
+
+            int secondNumerator = int.Parse(secondFraction[0]);
+            int secondDenominator = int.Parse(secondFraction[1]);
+            
+            MyFrac fracOfSecond = new MyFrac(secondNumerator, secondDenominator);
+            
+            Console.WriteLine($"\nДрiб з цiлою частиною:{fracOfFirst.ToStringWithIntegerPart()}");
+            Console.WriteLine($"Десяткове значення дробу: {fracOfFirst.DoubleValue()}");
+            Console.WriteLine($"Додавання дробiв: {fracOfFirst.Plus(fracOfSecond)}");
+            Console.WriteLine($"Вiднiмання дробiв: {fracOfFirst.Minus(fracOfSecond)}");
+            Console.WriteLine($"Множення дробiв: {fracOfFirst.Multiply(fracOfSecond)}");
+            Console.WriteLine($"Дiлення дробiв: {fracOfFirst.Divide(fracOfSecond)}");
+            Console.WriteLine($"Сума першого ряду дробiв (10 членiв): {fracOfFirst.CalcSumFirst(10)}");
+            Console.WriteLine($"Сума другого ряду дробiв (10 членiв): {fracOfFirst.CalcSumSecond(10)}");
+        }
+    }
+}
